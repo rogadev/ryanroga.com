58f6131906ceaabe363719b2637148990bf99167
vonroga <ryanroga@gmail.com>
2022-10-23 22:23:30 -0700

    Update index.svelte

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -5,7 +5,7 @@
     Contact
   </h1>
 
-  <p class="text-center text-gray-900 dark:text-white">
+  <p class="text-center text-gray-900 dark:text-white mt-4">
     Email me -
     <a class="hover:text-blue-500" href="mailto:ryan.roga@itas.ca"
       >ryan.roga@itas.ca</a
