2b7e7862a48516ddac05278dabf9fc22228b1f79
Roga <ryanroga@gmail.com>
2023-06-27 10:12:17 -0300

    Astro Pivot

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My personal website.';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const PARTYTOWN_SRC = 'to-be-determined.com';
+export const CITY = "Victoria";
+export const SITE_URL = "https://victoria.mossaway.ca";
