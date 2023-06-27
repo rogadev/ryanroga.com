ae831824bb82f5c3839044911cb3a3aec817a7ae
Roga <ryanroga@gmail.com>
2023-06-27 15:02:32 -0300

    Changes

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const PARTYTOWN_SRC = 'to-be-determined.com';
+export const PARTYTOWN_SRC = 'https://www.googletagmanager.com/gtag/js?id=G-7PEKMXXN4S';
 export const CITY = "Victoria";
 export const SITE_URL = "https://victoria.mossaway.ca";
