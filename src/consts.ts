8ee9d737bfa96f8ddee104a300438dc4ca6841aa
Roga <ryanroga@gmail.com>
2023-07-21 08:32:37 -0300

    Astro is out.

--- src/consts.ts
+++
@@ -1,8 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const PARTYTOWN_SRC = 'https://www.googletagmanager.com/gtag/js?id=G-7PEKMXXN4S';
-export const CITY = "Victoria";
-export const SITE_URL = "https://victoria.mossaway.ca";
