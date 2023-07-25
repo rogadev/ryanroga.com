f452113f10d7899fc3174db62862020b6c3bcabf
Roga <ryanroga@gmail.com>
2023-07-25 09:10:53 -0300

    Create UnderConstruction.svelte

---
+++ src/lib/components/UnderConstruction.svelte
@@ -0,0 +1,32 @@
+<div class="bg-white">
+	<div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+		<div class="relative overflow-hidden rounded-lg">
+			<div class="absolute inset-0">
+				<img
+					src="https://tailwindui.com/img/ecommerce-images/home-page-01-feature-section-01.jpg"
+					alt=""
+					class="h-full w-full object-cover object-center"
+				/>
+			</div>
+			<div class="relative bg-gray-900 bg-opacity-75 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
+				<div class="relative mx-auto flex max-w-3xl flex-col items-center text-center">
+					<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+						<span class="block sm:inline">This section is</span>
+						<span class="block sm:inline">still under construction</span>
+					</h2>
+					<p class="mt-3 text-xl text-white">
+						I'm in the middle of a pretty major overhaul of the site. I've changed frameworks, added
+						plans for more interactivity, as well as adding a devlog to blog about my projects and
+						progress. Please check back in a few days if something on this page is of interest to
+						you. Thank you for your patience.
+					</p>
+					<a
+						href="/"
+						class="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
+						>Head back home</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
