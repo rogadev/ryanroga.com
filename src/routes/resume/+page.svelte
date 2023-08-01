3c1df639354cfbf6d33b306f892af6f440d86cc2
Roga <ryanroga@gmail.com>
2023-08-01 11:09:48 -0300

    Stubbed Resume Page
    "Under construction"

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import UnderConstruction from '$lib/components/UnderConstruction.svelte';
+</script>
+
+<svelte:head>
+	<title>Contact | Roga.dev</title>
+	<meta name="description" content="Contact Roga.dev" />
+</svelte:head>
+
+<div>
+	<!-- TODO Finish the page -->
+	<UnderConstruction />
+</div>
