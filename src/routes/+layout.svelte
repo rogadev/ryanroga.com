4bbc147de32c75b8e3702821c8f82ff6f0ad3793
Roga <ryanroga@gmail.com>
2023-11-26 10:14:26 -0400

    Reworking In Progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
 	import '../app.postcss';
 	import '../styles.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <div class="flex flex-col min-h-screen bg-white">
 	<div class="h-full w-full">
+		<header>
+			<Navbar />
+		</header>
 		<slot />
+		<Footer />
 	</div>
 </div>
