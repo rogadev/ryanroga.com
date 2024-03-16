e8fbc0975bd5eb8babc8eeb9cb6d203a819f2d03
Roga <ryanroga@gmail.com>
2024-03-16 11:02:28 -0300

    Refactor layout component to include a background and isolate content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,19 @@
 <script>
-	import Navbar from '$lib/components/Navbar.svelte';
-	import Footer from '$lib/components/Footer.svelte';
 	import '../app.postcss';
 	import '../styles.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <div class="app bg-white">
 	<div class="h-full w-full">
-		<Navbar />
-		<slot />
+		<div class="bg-white">
+			<Navbar />
+
+			<div class="relative isolate px-6 pt-14 lg:px-8">
+				<slot />
+			</div>
+		</div>
 		<Footer />
 	</div>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-</style>
