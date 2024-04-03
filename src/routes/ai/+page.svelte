5ceb36f2a54ef383ce41328d41af78ddbdb5d74f
Roga <ryanroga@gmail.com>
2024-04-03 16:09:11 -0300

    Fill out the AI page with pricing content.

--- src/routes/ai/+page.svelte
+++ src/routes/ai/+page.svelte
@@ -1,3 +1,320 @@
-<div class="prose">
-	<h1>Coming Soon...</h1>
+<div class="bg-gray-900 py-24 sm:py-32 rounded-lg mt-6">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div class="mx-auto max-w-4xl text-center">
+			<h2 class="text-base font-semibold leading-7 text-orange-400">Pricing</h2>
+			<p class="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
+				AI Training For Teams & Individuals
+			</p>
+		</div>
+		<p class="mx-auto mt-6 max-w-3xl text-center text-lg leading-8 text-gray-300">
+			Unlock the transformative power of AI for your team with our comprehensive training. Explore
+			how AI can amplify productivity, enhance professional writing, and innovate problem-solving,
+			revolutionizing the way you and your team tackle challenges.
+		</p>
+		<div
+			class="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+		>
+			<div class="rounded-3xl p-8 xl:p-10 ring-1 ring-white/10">
+				<div class="flex items-center justify-between gap-x-4">
+					<h3 id="tier-freelancer" class="text-lg font-semibold leading-8 text-white">
+						One-to-One
+					</h3>
+				</div>
+				<p class="mt-4 text-sm leading-6 text-gray-300">
+					Get personalized attention and guidance with one-to-one individual training sessions.
+				</p>
+				<p class="mt-6 flex items-baseline gap-x-1">
+					<span class="text-4xl font-bold tracking-tight text-white">$250</span>
+					<span class="text-sm font-semibold leading-6 text-gray-300">/hour</span>
+				</p>
+				<a
+					href="https://calendly.com/ryan-awq/ai-office-training"
+					aria-describedby="tier-freelancer"
+					class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white"
+				>
+					Schedule Session
+				</a>
+				<ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Customized curriculum to fit your role and industry
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Direct access to an AI expert for immediate query resolution
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Opportunity to work on real-life projects relevant to your job
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Continuous assessment and feedback for consistent improvement
+					</li>
+				</ul>
+			</div>
+			<div class="rounded-3xl p-8 xl:p-10 bg-white/5 ring-2 ring-orange-500">
+				<div class="flex items-center justify-between gap-x-4">
+					<h3 id="tier-startup" class="text-lg font-semibold leading-8 text-white">
+						One-to-One Recurring
+					</h3>
+					<p
+						class="rounded-full bg-orange-500 px-2.5 py-1 text-xs font-semibold leading-5 text-white whitespace-nowrap"
+					>
+						Great Value
+					</p>
+				</div>
+				<p class="mt-4 text-sm leading-6 text-gray-300">
+					Experience ongoing growth and skill development with our recurring training sessions.
+				</p>
+				<p class="mt-6 flex items-baseline gap-x-1">
+					<!-- Price, update based on frequency toggle state -->
+					<span class="text-4xl font-bold tracking-tight text-white">$190</span>
+					<!-- Payment frequency, update based on frequency toggle state -->
+					<span class="text-sm font-semibold leading-6 text-gray-300">/hour</span>
+				</p>
+				<a
+					href="https://calendly.com/ryan-awq/ai-office-training"
+					aria-describedby="tier-startup"
+					class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-orange-500 text-white shadow-sm hover:bg-orange-400 focus-visible:outline-orange-500"
+					>Schedule Session</a
+				>
+				<ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Customized curriculum to fit your role and industry
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Direct access to an AI expert for immediate query resolution
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Opportunity to work on real-life projects relevant to your job
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Continuous assessment and feedback for consistent improvement
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Recurring sessions to reinforce and build upon previous learning
+					</li>
+				</ul>
+			</div>
+			<div class="rounded-3xl p-8 xl:p-10 ring-1 ring-white/10">
+				<div class="flex items-center justify-between gap-x-4">
+					<h3 id="tier-enterprise" class="text-lg font-semibold leading-8 text-white">
+						Group Training
+					</h3>
+				</div>
+				<p class="mt-4 text-sm leading-6 text-gray-300">
+					Collaborative learning experiences for teams and organizations.
+				</p>
+				<p class="mt-6 flex items-baseline gap-x-1">
+					<!-- Price, update based on frequency toggle state -->
+					<span class="text-4xl font-bold tracking-tight text-white">$500</span>
+					<!-- Payment frequency, update based on frequency toggle state -->
+					<span class="text-sm font-semibold leading-6 text-gray-300">/session*</span>
+				</p>
+				<div
+					aria-describedby="tier-enterprise"
+					class="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white/10 text-white focus-visible:outline-white hover:cursor-default"
+				>
+					Coming Soon...
+				</div>
+				<ul role="list" class="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Tailored training content to meet the collective needs of the team
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Interactive sessions that promote teamwork and problem-solving skills
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Scalable learning solutions for groups of various sizes
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Insights into how AI can streamline workflows and boost productivity
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Real-world case studies to illustrate the impact of AI in your industry
+					</li>
+					<li class="flex gap-x-3">
+						<svg
+							class="h-6 w-5 flex-none text-white"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						Access to group activities and exercises to reinforce learning outcomes
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
