2bca7286ef8cb2b8e7959c82837c193f3c554aaf
Roga <ryanroga@gmail.com>
2025-03-15 10:49:04 -0700

    close mobile nav menu on navigation

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -140,6 +140,7 @@
 							{#each tabs as { name, href }}
 								<a
 									{href}
+									on:click={() => (isMobileMenuOpen = false)}
 									class="relative group -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 									class:active={currentPath === href}
 								>
